e7527cb61f18636621ae09ddbee201f5dfb2a5ec
EmmanuelRoots <em.razanakoto@gmail.com>
2025-10-02 16:29:23 +0300

    add layout architecture

--- src/services/factory/navBar.factory.ts
+++ src/services/factory/navBar.factory.ts
@@ -4,8 +4,7 @@ import { logoFactory } from "./logo.factory"
 export const navBarFactory = ()=> {
   const navItems : NavItemProps [] = [
     {name:"ekandralogo",path:"/",type:"icon",logo:logoFactory(50,50)},
-    {name:"Home",path:"/",type:"text"},
-    {name : "Actualité",path:"/news",type:"text"},
+    {name : "Actualité",path:"/",type:"text"},
     {name:"Message",path:"/message",type:"text"}
   ]
 
