2b67d1161c01be566167f2590916d21368f8a1c0
EmmanuelRoots <em.razanakoto@gmail.com>
2025-10-02 16:33:51 +0300

    finishedSection layout

--- src/pages/message/index.tsx
+++ src/pages/message/index.tsx
@@ -1,9 +1,19 @@
+import { CenterSection, LeftSection, RightSection, SectionLayout } from "../layout/section"
+
 const MessagePage = ()=> {
 
   return (
-      <div>
-          <h1>Message page view</h1>
-      </div>
+      <SectionLayout>
+        <LeftSection style={{flex : "1 1 20%"}}>
+          <h1>Message liste</h1>
+        </LeftSection>
+        <CenterSection style={{flex : "1 1 60%"}}>
+          <h1>Message detail</h1>
+        </CenterSection>
+        <RightSection style={{flex : "1 1 20%"}}>
+          <h1>Message menu</h1>
+        </RightSection>
+      </SectionLayout>
   )
 }
 
