35607ca04803a8ab44747dfcfc411278b19bfce2
EmmanuelRoots <em.razanakoto@gmail.com>
2025-10-28 10:49:14 +0300

    finalize post 2

--- src/services/utils/role.utils.ts
+++ src/services/utils/role.utils.ts
@@ -1,4 +1,4 @@
-// hooks/permissions/canDoAction.ts
+import type { TABLE_NAME } from "../../constants/table.constant";
 import { UseAuth } from "../../context/user"
 import type { AuthorizationDto } from "../../data/dto/role";
 
@@ -9,16 +9,16 @@ export enum RoleAction {
   delete = "delete",
 }
 
-export const CanDoAction = (tableName: string, action: keyof typeof RoleAction): boolean => {
+export const CanDoAction = (tableName: keyof typeof TABLE_NAME, action: keyof typeof RoleAction): boolean => {
   const { user } = UseAuth()
-
+ 
   if (!user) return false
 
   const role = user.role
   if (!role) return false
 
   const auth: AuthorizationDto | undefined = role.authorizations.find(
-    (a) => a.tableName === tableName
+    (a) => a.tableName === tableName as string
   )
 
   if (!auth) return false
