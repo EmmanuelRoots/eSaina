0f5ffd7eed7742e12f53e962520f6dec6ff91f24
Bemsa <em.razanakoto@gmail.com>
2026-05-19 10:19:25 +0300

    fix: Update package.json with package manager details and format vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['jwt-decode'],
-    force: true
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
   },
-})
+  optimizeDeps: {
+    include: ["jwt-decode"],
+    force: true,
+  },
+});
